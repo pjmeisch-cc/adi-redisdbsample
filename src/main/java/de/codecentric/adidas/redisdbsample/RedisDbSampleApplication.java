69e339dfebb9ad10ca8f2ddd682b321437ba380c
P.J. Meisch <peter-josef.meisch@codecentric.de>
2018-01-22 13:19:34 +0100

    initial commit for sample program

---
+++ src/main/java/de/codecentric/adidas/redisdbsample/RedisDbSampleApplication.java
@@ -0,0 +1,47 @@
+package de.codecentric.adidas.redisdbsample;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class RedisDbSampleApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RedisDbSampleApplication.class);
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public RedisDbSampleApplication(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisDbSampleApplication.class, args);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        LOGGER.info("test called");
+
+        final ValueOperations<String, String> opsForValue = stringRedisTemplate.opsForValue();
+
+        final String key = "pjm-test";
+
+        if (!stringRedisTemplate.hasKey(key)) {
+            final String value = "success";
+            LOGGER.info("setting value {} for key {}", value, key);
+            opsForValue.set(key, value);
+        }
+
+        LOGGER.info("getting value for key {}", key);
+        String value = opsForValue.get(key);
+        LOGGER.info("got value {}", value);
+        return value;
+    }
+}
---
+++ src/test/java/de/codecentric/adidas/redisdbsample/RedisDbSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package de.codecentric.adidas.redisdbsample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisDbSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
