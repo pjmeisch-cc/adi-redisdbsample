effbe50ec26a2071327687e6c2725e3b34b0614b
P.J. Meisch <peter-josef.meisch@codecentric.de>
2018-01-22 15:16:44 +0100

    fix configuration

--- src/main/java/de/codecentric/adidas/redisdbsample/RedisDbSampleApplication.java
+++ src/main/java/de/codecentric/adidas/redisdbsample/RedisDbSampleApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +18,13 @@ public class RedisDbSampleApplication {
 
     private final StringRedisTemplate stringRedisTemplate;
 
-    public RedisDbSampleApplication(StringRedisTemplate stringRedisTemplate) {
+    private final Environment environment;
+
+    public RedisDbSampleApplication(StringRedisTemplate stringRedisTemplate,
+                                    Environment environment) {
         this.stringRedisTemplate = stringRedisTemplate;
+        this.environment = environment;
+        LOGGER.info("redis host: {}", environment.getProperty("spring.redis.host"));
     }
 
     public static void main(String[] args) {
